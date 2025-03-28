a6dac2d4d4e20d04439a936543f9585b10b3b54b
Ahmed <esqeue@gmail.com>
2025-03-29 05:41:19 +0700

    HeroUI setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,17 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import { HeroUIProvider } from "@heroui/react";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+    <HeroUIProvider>
+      <Html lang="en" className="dark text-foreground bg-background">
+        <Head />
+        <body className="antialiased">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    </HeroUIProvider>
   );
 }
+
