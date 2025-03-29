199a818e89a2267b9228075800e4a81f37778e43
Ahmed <esqeue@gmail.com>
2025-03-29 16:21:08 +0700

    fix hydration error

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -6,10 +6,6 @@ import { Moon, Sun } from "lucide-react";
 export const ThemeSwitcher = () => {
   const { theme, setTheme } = useTheme()
 
-  const handleClickSwitch = () => {
-
-  }
-
   return (
     <div>
       <Button isIconOnly onPress={() => theme === "light" ? setTheme("dark") : setTheme("light")}>
