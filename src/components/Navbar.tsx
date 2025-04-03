9480882bb711cd5e9af45d21569e366108545fe2
Ahmed <esqeue@gmail.com>
2025-04-03 15:22:06 +0700

    refine logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from "@heroui/react";
 import { ThemeSwitcher } from "./ThemeSwitcher";
+import Image from "next/image";
 
-export default function DefaultNavbar() {
+export default function DefaultNavbar(props: {currentTheme: string}) {
   return (
     <>
       <Navbar position="static">
         <NavbarBrand>
-          <p className="satoshi text-3xl text-inherit">Randus</p>
+          <Image width={140} height={0} src="/randus-text.svg" alt="randus icon" className={props.currentTheme === "blurple-dark" ? "invert" : ""}></Image>
         </NavbarBrand>
         <NavbarContent className="sm:flex gap-4 hide" justify="center">
         </NavbarContent>
